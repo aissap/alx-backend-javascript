0d7296d65cf83621a3cfd665c968f5ad1e7c86c4
aissap <jesus.jesus.core@gmail.com>
2024-04-23 20:42:55 +0200

    fixing packages

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,14 @@
-function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-function getLast() {
-    return ' is okay';
-  }
-function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
-  module.exports = {
-    taskFirst,
-    taskNext
-};
+export function taskFirst() {
+	const task = 'I prefer const when I can.';
+	return task;
+	}
+export function getLast() {
+	return ' is okay';
+	}
+export function taskNext() {
+	let combination = 'But sometimes let';
+	combination += getLast();
+
+	return combination;
+	}
+
