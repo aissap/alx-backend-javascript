4dab653fa3159e6452d46a5272219d88953001a4
aissap <jesus.jesus.core@gmail.com>
2024-04-24 23:16:14 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulating API call
-    setTimeout(() => {
-      resolve("Response from API");
-    }, 1000);
-  });
+  return new Promise(() => {});
 }
-
