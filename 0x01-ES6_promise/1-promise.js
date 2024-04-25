21eb238387c00dc302bc847539f57c3d78b9bb43
aissap <jesus.jesus.core@gmail.com>
2024-04-25 01:49:03 +0000

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,10 @@
+// Return a promise based on the boolean parameter
 export default function getFullResponseFromAPI(success) {
-	return new Promise((resolve, reject) => {
-		if (success) {
-			resolve({
-				status: 200,
-				body: 'Success',
-			});
-		} else {
-			reject(new Error('The fake API is not working currently'));
-		}
-	});
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
