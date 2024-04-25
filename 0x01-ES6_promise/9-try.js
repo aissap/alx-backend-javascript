b9f9303efe2b3333ed3c25fdba1ea7c8fa02d91c
aissap <jesus.jesus.core@gmail.com>
2024-04-25 01:22:20 +0000

    9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,10 +4,9 @@ export default function guardrail(mathFunction) {
 
 	try {
 		queue.push(mathFunction);
-	} catch (error) {
-		queue.push(String(error));
-	} finally
-	{
+	} catch (err) {
+		queue.push(String(err));
+	} finally{
 		queue.push('Guardrail was processed');
 	}
 
