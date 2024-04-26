cce1a6a4bad27e32ba042a382b04bb59e9d34e16
aissap <jesus.jesus.core@gmail.com>
2024-04-26 16:55:30 +0000

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,4 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
   return [19, 20, 34].map(size => new ClassRoom(size));
