2d25d5cc7c8ff31e613a1c6dbae2ddd17270a7f6
aissap <jesus.jesus.core@gmail.com>
2024-04-29 22:27:07 +0000

    100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+const weakMap = new WeakMap();
+
+export { weakMap };
+
+export function queryAPI(endpoint) {
+  let count = weakMap.get(endpoint) || 0;
+  count++;
+
+  // Update the count in the weakMap
+  weakMap.set(endpoint, count);
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  return count;
+}
