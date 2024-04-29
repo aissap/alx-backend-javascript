86752b73c1f9027b913ec55948a950744c75b0ec
aissap <jesus.jesus.core@gmail.com>
2024-04-29 22:07:41 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,15 @@
 export default function cleanSet(set, startString) {
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  return filteredValues.join('-');
+  // Check if input parameters are valid
+  if (!set || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  const cleanedValues = [];
+  for (const value of set) {
+    // Check if the value starts with the specified startString
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const cleanedValue = value.substring(startString.length);
+      cleanedValues.push(cleanedValue);
+    }
+  }
+  return cleanedValues.join('-');
 }
