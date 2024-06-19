57673dfaf4d284adf37241188e6e07f3bf64367d
aissap <jesus.jesus.core@gmail.com>
2024-06-19 02:07:08 +0200

    task 0 and 1 ✅

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    const input = data.toString().trim();
+    const input = data.trim();
 
     if (input.length > 0) {
         process.stdout.write(`Your name is: ${input}\n`);
-        process.exit();
+        process.exit(0);
     }
-});
-
-process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
 });
