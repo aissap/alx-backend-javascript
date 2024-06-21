3ac79df311f3f99039ef51153d32d877a7cc851c
aissap <jesus.jesus.core@gmail.com>
2024-06-22 00:43:14 +0200

    .\1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,6 +5,7 @@ process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${name}\n`);
   process.stdin.end();
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
