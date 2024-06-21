09e98a791bfee728cbce41b02f1ea0ec8656959a
aissap <jesus.jesus.core@gmail.com>
2024-06-21 02:03:14 +0200

    task 7??

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -9,7 +9,7 @@ describe('Testing numbers', () => {
     expect(2 === 2).to.be.true;
   });
 
-  it('1 is equal to 3', () => {
+  it.skip('1 is equal to 3', () => {
     expect(1 === 3).to.be.true;
   });
 
@@ -29,7 +29,7 @@ describe('Testing numbers', () => {
     expect(6 === 6).to.be.true;
   });
 
-  it.skip('7 is equal to 7', () => {
+  it('7 is equal to 7', () => {
     expect(7 === 7).to.be.true;
   });
 });
