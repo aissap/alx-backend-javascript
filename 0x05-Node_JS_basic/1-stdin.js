c879e346fdd62be6c09038f53909fd9ed5ae50c8
aissap <jesus.jesus.core@gmail.com>
2024-06-21 23:57:02 +0200

    .\1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,17 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const readline = require('readline');
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-
-  if (name !== null) {
-    process.stdout.write(`Your name is: ${name}`);
-  }
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
 });
