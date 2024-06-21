4623e44481d91c443c632615671376d6472d621b
aissap <jesus.jesus.core@gmail.com>
2024-06-22 00:08:35 +0200

    .\1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,4 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.setEncoding('utf8');
-
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
