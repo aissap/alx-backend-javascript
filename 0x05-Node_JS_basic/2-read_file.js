20a5442827cb0b23a31ee4d28b42342166ed11ca
aissap <jesus.jesus.core@gmail.com>
2024-06-22 02:53:17 +0200

    task -2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,36 +2,31 @@ const fs = require('fs');
 
 /**
  * Counts the students in the CSV data file.
+ * @param {String} path The path to the CSV data file.
  */
-function countStudents (path) {
+function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
 
   const fileContent = fs.readFileSync(path, 'utf8');
-  const linz = fileContent.split('\n');
-  const hasht = {};
-  let students = -1;
-  for (const l of linz) {
-    if (l.trim() !== '') {
-      const colm = l.split(',');
-      const field = colm[3];
-      const fname = colm[0];
-      if (students >= 0) {
-        if (!Object.hasOwnProperty.call(hasht, field)) {
-          hasht[field] = [];
-        }
-        hasht[field] = [...hasht[field], fname];
+  const lines = fileContent.split('\n').filter(line => line.trim() !== '');
+
+  const students = { CS: [], SWE: [] };
+
+  lines.forEach(line => {
+    const [firstname, lastname, age, field] = line.split(',').map(item => item.trim());
+    if (firstname && lastname && age && field) {
+      if (field === 'CS' || field === 'SWE') {
+        students[field].push(firstname);
       }
-      students += 1;
     }
-  }
-  console.log(`Number of students: ${students}`);
-  for (const key in hasht) {
-    if (Object.hasOwnProperty.call(hasht, key)) {
-      console.log(`Number of students in ${key}: ${hasht[key].length}. List: ${hasht[key].join(', ')}`);
-    }
-  }
+  });
+
+  const totalStudents = students.CS.length + students.SWE.length;
+  console.log(`Number of students: ${totalStudents}`);
+  console.log(`Number of students in CS: ${students.CS.length}. List: ${students.CS.join(', ')}`);
+  console.log(`Number of students in SWE: ${students.SWE.length}. List: ${students.SWE.join(', ')}`);
 }
 
 module.exports = countStudents;
